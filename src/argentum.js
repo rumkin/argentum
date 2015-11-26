bc2b2cf42fcea2be4a2cafea5be0e754d41b4fe6
Rumkin <dev@rumk.in>
2015-11-26 06:04:15 +0000

    add extended array parsing

--- src/argentum.js
+++ src/argentum.js
@@ -1,51 +1,46 @@
 'use strict';
 
-module.exports = argentum;
+module.exports.parse = parse;
+module.exports.parseValue = parseValue;
+
 
 var rule = /^--([a-zA-Z0-9][a-zA-Z0-9_-]*)(\[])?(=(.+))?$/;
 
 /**
- * Convert array of options into object.
+ * Parse array of options into object.
  *
  * @param  {string[]} argv Array of string.
  * @return {object}      Parsed params.
  */
-function argentum(argv) {
+function parse(argv) {
   var opts = {};
+  var current;
+  var currentIsArray = false;
 
   argv.slice().forEach(function(arg, i){
     var match = arg.match(rule);
     var value, name;
+
     if (match) {
       name = match[1].replace(/-(.)/g, function (m, v) {
         return v.toUpperCase();
       });
 
       value = typeof match[4] !== 'undefined' ? match[4] : 'true';
+      current = name;
+      currentIsArray = false;
+
       var isArray = (match[2] === '[]');
       if (isArray && typeof match[4] === 'undefined') {
         // Skip empty array options like --users[].
+        currentIsArray = true;
+        opts[name] = [];
         return;
       } else if (isArray && ! Array.isArray(opts[name])) {
         opts[name] = [];
       }
 
-      switch (value.toLowerCase()) {
-        case 'true':
-          value = true;
-          break;
-        case 'false':
-          value = false;
-          break;
-        default:
-          if (value.match(/^-?\d+$/)) {
-            value = parseInt(value, 10);
-          } else if (value.match(/^-?\d+\.\d+$/)) {
-            value = parseFloat(value, 10);
-          } else {
-            value = value;
-          }
-      }
+      value = parseValue(value);
 
       argv.splice(i, 1);
 
@@ -54,8 +49,36 @@ function argentum(argv) {
       } else {
         opts[name] = value;
       }
+    } else if (currentIsArray) {
+      argv.splice(i, 1);
+      opts[current].push(parseValue(arg));
     }
   });
 
   return opts;
+}
+
+/**
+ * Parse argument value into js object. It parses booleans, integers and strings.
+ *
+ * @param  {string} value String value.
+ * @return {*}       Parsed JS value.
+ */
+function parseValue(value) {
+  switch (value.toLowerCase()) {
+  case 'true':
+    value = true;
+    break;
+  case 'false':
+    value = false;
+    break;
+  default:
+    if (value.match(/^-?\d+$/)) {
+      value = parseInt(value, 10);
+    } else if (value.match(/^-?\d+\.\d+$/)) {
+      value = parseFloat(value, 10);
+    }
+  }
+
+  return value;
 }
--- test/argentum.spec.js
+++ test/argentum.spec.js
@@ -3,78 +3,100 @@ var argentum = require('../src/argentum.js');
 
 describe('Argentum argv parser', function () {
   it('Should return object on parse', function () {
-    var result = argentum([]);
+    var result = argentum.parse([]);
     test.object(result);
   });
 
   it('Should convert name to camelCase', function () {
-    var result = argentum(['--test-value']);
+    var result = argentum.parse(['--test-value']);
 
     test.object(result).hasProperty('testValue');
   });
 
   it('Should parse empty rule as boolean true', function () {
-    var result = argentum(['--test']);
+    var result = argentum.parse(['--test']);
 
     test.object(result).hasProperty('test', true);
   });
 
   it('Should parse "true" as boolean true', function () {
-    var result = argentum(['--test=true']);
+    var result = argentum.parse(['--test=true']);
 
     test.object(result).hasProperty('test', true);
   });
 
   it('Should parse "false" as boolean false', function () {
-    var result = argentum(['--test=false']);
+    var result = argentum.parse(['--test=false']);
 
     test.object(result).hasProperty('test', false);
   });
 
   it('Should parse integer', function () {
-    var result = argentum(['--test=10']);
+    var result = argentum.parse(['--test=10']);
 
     test.object(result).hasProperty('test', 10);
   });
 
   it('Should parse negative integer', function () {
-    var result = argentum(['--test=-10']);
+    var result = argentum.parse(['--test=-10']);
 
     test.object(result).hasProperty('test', -10);
   });
 
   it('Should parse float', function () {
-    var result = argentum(['--test=1.3']);
+    var result = argentum.parse(['--test=1.3']);
 
     test.object(result).hasProperty('test', 1.3);
   });
 
   it('Should parse negative float', function () {
-    var result = argentum(['--test=-1.3']);
+    var result = argentum.parse(['--test=-1.3']);
 
     test.object(result).hasProperty('test', -1.3);
   });
 
   it('Should parse braces as array', function () {
-    var result = argentum(['--test[]=1']);
+    var result = argentum.parse(['--test[]=1']);
 
     test.object(result).hasProperty('test');
     test.array(result.test).hasProperty(0, 1);
   });
 
-  it('Should insert multiple values into one array', function () {
-    var result = argentum(['--test[]=1', '--test[]=2']);
+  it('Should overwrite non-array value', function () {
+    var result = argentum.parse(['--test=1', '--test[]=2']);
 
     test.object(result).hasProperty('test');
-    test.array(result.test).hasProperty(0, 1);
-    test.array(result.test).hasProperty(1, 2);
+    test.array(result.test).hasProperty(0, 2);
+  });
+
+  it('Should insert multiple values into array', function () {
+    var result = argentum.parse(['--test[]=1', '--test[]=2']);
+
+    test.object(result).hasProperty('test');
+    test.array(result.test)
+      .hasProperty(0, 1)
+      .hasProperty(1, 2);
   });
 
   it('Should cut matched arguments from source array', function () {
     var args = ['1', 'hello', '--hello=1', 'null'];
-    var result = argentum(args);
+    var result = argentum.parse(args);
 
     test.object(result).hasProperty('hello', 1);
     test.object(args).hasProperty('length', 3);
+  });
+
+  it('Should collect bracket properties', function() {
+    var args = ['--arr[]', '1', 'true', 'hello', '--not-arr'];
+    var result = argentum.parse(args);
+
+    test.object(result).hasProperty('arr');
+    test.object(result).hasProperty('notArr', true);
+    test.array(result.arr)
+      .hasProperty('length', 3)
+      .hasProperty('0', 1)
+      .hasProperty('1', true)
+      .hasProperty('2', 'hello')
+      ;
   });
 });
